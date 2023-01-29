d5553b968576de9373dd1b9e447f34f61797c43d
kingofkings1307 <112233804+kingofkings1307@users.noreply.github.com>
2023-01-29 22:51:42 +0530

    Create odd3 foc..cpp

---
+++ odd3 foc..cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,number;
+	printf("\n please enter the maximum limit value:");
+	scanf("%d",&number);
+	printf("\odd number between 1 and %d are:\n",number);
+	for(i=1;i<=number,i++)
+	{
+		if(i%2!=0)
+		{
+			printf("%d\t",i;)
+		}
+	}
+	return 0;
+}
+0 comments on comm
